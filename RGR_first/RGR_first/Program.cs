c87a135b16f097bb1eb25b503ef5c63c3d182896
Anton Makarenko <demo6112@gmail.com>
2018-05-12 16:47:46 +0300

    initial

---
+++ RGR_first/RGR_first/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RGR_first
+{
+    class Program
+    {
+        // a = mах(MB*MC+ ММ)
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
