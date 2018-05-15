3d0e399efd1f9d222b77c8371eecdb34f6218008
Anton Makarenko <demo6112@gmail.com>
2018-05-16 01:59:47 +0300

    delete unnecessary code

--- RGR_first/RGR_first/Utils.cs
+++ RGR_first/RGR_first/Utils.cs
@@ -6,7 +6,6 @@ namespace RGR_first
 {
     static class Utils
     {
-        private static object printMatrixLocker = new object();
         public static int[] GetVectorOfOne(int size)
         {
             int[] result = new int[size];
@@ -24,59 +23,5 @@ namespace RGR_first
             }
             return result;
         }
-
-        public static int[] GetVectorOfZero(int size)
-        {
-            int[] result = new int[size];
-            for (int i = 0; i < size; i++) {
-                result[i] = 0;
-            }
-            return result;
-        }
-
-        public static int[][] GetMatrixOfZero(int size)
-        {
-            int[][] result = new int[size][];
-            for (int i = 0; i < size; i++) {
-                result[i] = GetVectorOfOne(size);
-            }
-            return result;
-        }
-
-        public static int[] GetRandomVector(int size)
-        {
-            var random = new Random();
-            int[] result = new int[size];
-            for (int i = 0; i < size; i++) {
-                result[i] = random.Next(0, 10);
-            }
-            return result;
-        }
-
-        public static int[][] GetRandomMatrix(int size)
-        {
-            int[][] result = new int[size][];
-            for (int i = 0; i < size; i++) {
-                result[i] = GetRandomVector(size);
-            }
-            return result;
-        }
-
-        public static void PrintVector(int[] vector)
-        {
-            var str = string.Join(", ", vector);
-            Console.WriteLine(str);
-        }
-
-        public static void PrintMatrix(int[][] matrix, string matrixName)
-        {
-            lock (printMatrixLocker) {
-                Console.WriteLine(matrixName);
-                foreach (var i in matrix) {
-                    PrintVector(i);
-                }
-            }
-        }
-
     }
 }
